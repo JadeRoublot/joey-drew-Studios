b30e3514961e435e7d0a10748cf725af4bf37b07
TheCrow <77794984+JadeRoublot@users.noreply.github.com>
2022-12-19 21:39:54 +0100

    rajout de variable dans movieComponent

--- src/app/movie/movie.component.ts
+++ src/app/movie/movie.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class MovieComponent implements OnInit {
 
+  @Input() movieTitle?: string;
+  @Input() movieReleaseDate?: string;
+  @Input() movieProductor?: string;
+  @Input() movieRealisator?: string;
+  @Input() movieSynopsis?: string;
+  @Input() movieMusic?: string;
+  @Input() id?: string ;
+
   constructor() { }
 
   ngOnInit(): void {
