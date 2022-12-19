a27358c4cd7022fc4fc1fd0bf91adc7d40fa7ab4
TheCrow <77794984+JadeRoublot@users.noreply.github.com>
2022-12-19 21:22:17 +0100

    ajout du component movie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VinylComponent } from './vinyl/vinyl.component';
+import { MovieComponent } from './movie/movie.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    VinylComponent
+    VinylComponent,
+    MovieComponent
   ],
   imports: [
     BrowserModule,
