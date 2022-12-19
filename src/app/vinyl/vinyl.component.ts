f5a3257c606d48d28cf2ba4101d80f1ccfe80870
TheCrow <77794984+JadeRoublot@users.noreply.github.com>
2022-12-19 21:05:42 +0100

    Ajout du component vinyl

---
+++ src/app/vinyl/vinyl.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vinyl',
+  templateUrl: './vinyl.component.html',
+  styleUrls: ['./vinyl.component.scss']
+})
+export class VinylComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
