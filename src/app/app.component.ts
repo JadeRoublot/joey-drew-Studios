ada5b43e1d569827fe9b70b4c1b92f1cb483436d
TheCrow <77794984+JadeRoublot@users.noreply.github.com>
2022-12-22 15:40:35 +0100

    Update
    affichage par list present dans app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'joey-drew-Studios';
+
+  movies = [
+    {
+      title: 'Tasty Trio Troubles',
+      releaseDate: '06/03/1935',
+      productor: 'Joey Drew',
+      realisator: 'Joey Drew',
+      synopsis: 'Bendy gets a taste of the Butcher Gang and little else in this beloved classic cartoon from 1935.',
+      musicComposeur: 'Sammy Lawrence',
+      link: 'https://youtu.be/MX9x1oCGzX0',
+      affiche: '...',
+      nature: true
+
+    }
+  ];
+
+  vinyls = [
+    {
+      title: 'Tasty Trio Troubles Sound Soundtrack',
+      releaseDate: '06/03/1935',
+      productor: 'Sammy Lawrence',
+      movieUsing: 'Tasty Trio Troubles',
+      link: '...',
+      tour: false
+
+    }
+
+  ];
+
 }
