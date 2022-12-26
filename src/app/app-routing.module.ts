f58dd7491ba6cfba8d47c4187d71bf4b1c8529c7
TheCrow <77794984+JadeRoublot@users.noreply.github.com>
2022-12-26 16:13:50 +0100

    Implementation des routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { VinylListComponent } from './vinyl-list/vinyl-list.component';
+
+const routes: Routes = [
+  {
+    path: 'movie',
+    component: MovieListComponent
+  },
+  {
+    path: 'vinyl',
+    component: VinylListComponent
+  },
+  {
+    path: '',
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
