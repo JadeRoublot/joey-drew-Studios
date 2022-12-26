e50de235023642abc4a4ca458959910c130b51c5
TheCrow <77794984+JadeRoublot@users.noreply.github.com>
2022-12-26 23:37:43 +0100

    Update movie.service.ts

--- src/app/services/movie/movie.service.ts
+++ src/app/services/movie/movie.service.ts
@@ -20,6 +20,45 @@ export class MovieService {
       affiche: 'https://i.pinimg.com/originals/7a/57/c1/7a57c177ee7c2a5e96f44df3f49088b6.png',
       nature: true
 
+    },
+    {
+      id: 2,
+      title: 'Snow Sillies',
+      releaseDate: '12/25/1935',
+      productor: 'Joey Drew',
+      realisator: 'Joey Drew',
+      synopsis: 'A mini short from back in the heyday of Joey Drew Studios.',
+      musicComposeur: 'Sammy Lawrence',
+      link: 'https://www.youtube.com/watch?v=3RmZDZja4Hs',
+      affiche: 'https://i.pinimg.com/originals/7a/57/c1/7a57c177ee7c2a5e96f44df3f49088b6.png',
+      nature: true
+
+    },
+    {
+      id: 3,
+      title: 'The Dancing Demon',
+      releaseDate: '01/01/1930',
+      productor: 'Joey Drew',
+      realisator: 'Henry Stein',
+      synopsis: 'A reproduction of the really first movie of joey drew studios',
+      musicComposeur: 'Sammy Lawrence',
+      link: '. . .',
+      affiche: 'https://i.pinimg.com/originals/bc/18/a7/bc18a7bf8457627e346b10464bcd20ed.jpg',
+      nature: false
+
+    },
+    {
+      id: 4,
+      title: 'The Sheep Song',
+      releaseDate: '02/13/1930',
+      productor: 'Joey Drew',
+      realisator: 'Henry Stein',
+      synopsis: 'Here is the first movie who ever feature our favorite wolf Boris.',
+      musicComposeur: 'Sammy Lawrence',
+      link: '. . .',
+      affiche: 'https://i.pinimg.com/originals/0a/9e/1d/0a9e1db10549bfc5dfcba400388e2ef0.jpg',
+      nature: false
+
     }
     
   ];
