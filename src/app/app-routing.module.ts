52b68e853b0d9c8e08d69c7853cfd2e85c526b42
TheCrow <77794984+JadeRoublot@users.noreply.github.com>
2022-12-26 17:33:59 +0100

    Import des routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,15 @@ import { HomeComponent } from './home/home.component';
 import { MovieListComponent } from './movie-list/movie-list.component';
 import { VinylListComponent } from './vinyl-list/vinyl-list.component';
 
+import { MovieNewComponent } from './movie-new/movie-new.component';
+import { VinylNewComponent } from './vinyl-new/vinyl-new.component';
+
+import { MovieModifComponent } from './movie-modif/movie-modif.component';
+import { VinylModifComponent } from './vinyl-modif/vinyl-modif.component';
+
+import { MovieLearnMoreComponent } from './movie-learn-more/movie-learn-more.component';
+import { VinylLearnMoreComponent } from './vinyl-learn-more/vinyl-learn-more.component';
+
 const routes: Routes = [
   {
     path: 'movie',
