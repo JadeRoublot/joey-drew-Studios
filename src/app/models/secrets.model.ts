b47a1aedbacb4999e809d8e229e21d87331aa897
TheCrow <77794984+JadeRoublot@users.noreply.github.com>
2023-01-03 19:29:43 +0100

    [ACCESS DENIED]
    [ACCESS DENIED]

--- src/app/models/secrets.model.ts
+++ src/app/models/secrets.model.ts
@@ -1,2 +1,23 @@
 export class Secrets {
+
+    id?: string;
+    title: string;
+    nickname: string;
+    nameOfSubject: string;
+    objectifImgLink: string;
+    resultImgLink: string;
+    killCount: string;
+    log: string;
+
+    constructor() {
+
+        this.title = '';
+        this.nickname = '';
+        this.nameOfSubject = '';
+        this.objectifImgLink = '';
+        this.resultImgLink = '';
+        this.killCount = '';
+        this.log = '';
+
+    }
 }
